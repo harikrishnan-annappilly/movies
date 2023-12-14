5812206de0d3c51e270a0376408f3c4aaa21349d
Hari <harikrishnan.annappilly@gmail.com>
2023-12-14 21:29:48 +0530

    Backend: DB config created

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
 
+from db import db
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+@app.before_request
+def create_tables():
+    db.create_all()
 
 
 @app.route('/')
