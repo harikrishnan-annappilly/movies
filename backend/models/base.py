b0bdb424e961f13b045ecc5da79552a62744c04f
Hari <harikrishnan.annappilly@gmail.com>
2023-12-15 07:54:27 +0530

    Backend: Refactroed helper functions to have single logic, Added abstract to BaseModel

--- backend/models/base.py
+++ backend/models/base.py
@@ -2,6 +2,7 @@ from db import db
 
 
 class BaseModel(db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
 
     def save(self):
--- backend/util/helper.py
+++ backend/util/helper.py
@@ -1,27 +1,40 @@
 from models import BaseModel
 
 
-def find_or_404(Model: BaseModel, **kwargs):
+def _check_item(Model: BaseModel, need_item: bool, **kwargs):
+    """
+    This method will check and let you know whether item present or not for the passed Args.
+    need_item => used for checking whether to throw error when item is present or not.
+    """
+
     def wrapper(func):
         def inner(*func_args, **func_kwargs):
             model = Model.find_one(**kwargs)
-            if model is None:
-                return {'message': 'item not found', 'table': Model.__tablename__, 'data': kwargs}
-            return func(*func_args, model, **func_kwargs)
-
-        return inner
-
-    return wrapper
-
-
-def if_exist_400(Model: BaseModel, **kwargs):
-    def wrapper(func):
-        def inner(*func_args, **func_kwargs):
-            model = Model.find_one(**kwargs)
-            if model:
-                return {'message': 'item already taken', 'table': Model.__tablename__, 'data': kwargs}
+            error_data = (
+                {'message': 'item not found', 'code': 404}
+                if need_item
+                else {'message': 'item already taken', 'code': 400}
+            )
+            if model is None if need_item else model:
+                return {
+                    'message': error_data['message'],
+                    'table': Model.__tablename__,
+                    'data': kwargs,
+                }, error_data['code']
+            if need_item:
+                return func(*func_args, model, **func_kwargs)
             return func(*func_args, **func_kwargs)
 
         return inner
 
     return wrapper
+
+
+def find_or_404(Model: BaseModel, **kwargs):
+    need_item = True  # We need item to be present in DB
+    return _check_item(Model, need_item, **kwargs)
+
+
+def if_exist_400(Model: BaseModel, **kwargs):
+    need_item = False  # We need item not present in DB
+    return _check_item(Model, need_item, **kwargs)
