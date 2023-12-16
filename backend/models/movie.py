94c968965f5380f035762f1cb203456baf878cbe
Hari <harikrishnan.annappilly@gmail.com>
2023-12-16 11:16:53 +0530

    Backend: Issue with category delete while movie assiged

--- backend/models/movie.py
+++ backend/models/movie.py
@@ -7,7 +7,7 @@ from . import CategoryModel
 class MovieModel(BaseModel):
     name = db.Column(db.String(20), nullable=False, unique=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category_model.id'), nullable=False)
-    category = db.relationship('CategoryModel', backref='movies')
+    category = db.relationship('CategoryModel', backref=db.backref("movies", cascade="all, delete-orphan", lazy=True))
     category: Mapped[CategoryModel]
 
     def json(self) -> dict:
