7dc2fbf4615c6f38d27a84723712c01dab17f65e
Hari <harikrishnan.annappilly@gmail.com>
2023-12-20 21:40:04 +0530

    Frontend: Created first layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="row">
+                <div className="col-12 bg-danger">NavBar</div>
+            </div>
+            <div className="row">
+                <div className="d-none d-md-block col-12 col-md-4 col-lg-3 bg-primary">
+                    Category
+                </div>
+                <div className="col-12 col-md-8 col-lg-9 bg-success">
+                    Movies
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
